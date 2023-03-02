dff564a527c766b77f1bfb1a14544ac08a5aa295
mygitgor <mypywelcome@gmail.com>
2023-03-02 21:18:42 +0400

    addedServer

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("it's main Server");
+            OurServer server = new OurServer();
+        }
+
+    }
+}
