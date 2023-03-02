4cd16aad6fa25d3421712f62a2daec124dc14c8c
mygitgor <mypywelcome@gmail.com>
2023-03-02 21:19:14 +0400

    addedClient

---
+++ OurClient.cs
@@ -0,0 +1,36 @@
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    class OurClient
+    {
+        private TcpClient client;
+        private StreamWriter sWriter;
+    
+
+    public OurClient()  
+    {
+        client = new TcpClient("127.0.0.1", 5555);
+        sWriter = new StreamWriter(client.GetStream(), Encoding.UTF8);  
+
+        HandleCommunication();
+    }
+
+    void HandleCommunication()
+    {
+        while (true)
+        {
+            Console.Write("> ");
+            string massage = Console.ReadLine();
+            sWriter.WriteLine(massage);
+            sWriter.Flush();
+        }
+    }
+    }
+    
+    
+
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("it's main Client");
+            OurClient ourClient = new OurClient();
+        }
+
+    }
+}
